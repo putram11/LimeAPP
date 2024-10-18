52688331633eaf217f70fcfed82bef14c0e8df2f
Putra Mahardika <putram.works@gmail.com>
2024-10-18 13:24:12 +0700

    Update resolvers.js

--- server/user/resolvers.js
+++ server/user/resolvers.js
@@ -4,18 +4,24 @@ const { comparePassword } = require("../helpers/bcrypt");
 
 const userResolvers = {
   Query: {
+    // Get user by ID
     getUserById: async (_, { id }, { user }) => {
-      if (!user) throw new Error("Authentication required");
+      // Check if user is authenticated
+      if (!user) {
+        throw new Error("Authentication required");
+      }
 
       return await UserModel.findById(id);
     },
 
+    // Search users by keyword
     searchUsers: async (_, { keyword }) => {
       return await UserModel.searchUsers(keyword);
     },
   },
 
   Mutation: {
+    // Register a new user
     registerUser: async (_, { name, username, email, password }) => {
       const newUser = {
         name,
@@ -23,21 +29,24 @@ const userResolvers = {
         email,
         password,
       };
+      
       return await UserModel.create(newUser);
     },
 
+    // Log in an existing user
     loginUser: async (_, { username, password }) => {
       const user = await UserModel.findByUsername(username);
       if (!user) {
         throw new Error("User not found");
       }
 
+      // Validate password
       const validPassword = comparePassword(password, user.password);
       if (!validPassword) {
         throw new Error("Invalid password");
       }
 
-      // Buat token JWT
+      // Generate JWT token
       const token = generateToken({
         userId: user._id,
         username: user.username,
