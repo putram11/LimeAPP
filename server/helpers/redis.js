fff2209d2499d3f98b14a75ed8d3eddf3cbe005a
Putra Mahardika <putram.works@gmail.com>
2024-10-18 13:11:38 +0700

    Update redis.js

--- server/helpers/redis.js
+++ server/helpers/redis.js
@@ -1,6 +1,7 @@
 // redisClient.js
 const { createClient } = require("redis");
 
+// Create a Redis client instance
 const client = createClient({
   password: process.env.REDIS_PASS,
   socket: {
@@ -9,21 +10,26 @@ const client = createClient({
   },
 });
 
-// Error handling
+// Error handling for the Redis client
 client.on("error", (err) => {
   console.error("Redis Client Error:", err);
 });
 
-// Fungsi untuk menghubungkan ke Redis
+/**
+ * Connects to the Redis database.
+ * @returns {Promise<void>}
+ */
 const connectRedis = async () => {
   try {
     await client.connect();
     console.log("Connected to Redis");
   } catch (error) {
     console.error("Failed to connect to Redis:", error);
+    throw error; // Optional: propagate error for further handling
   }
 };
 
+// Initiate connection to Redis
 connectRedis();
 
 module.exports = client;
