6c6d9ba884a691072b814cd9c8626c3cdecac10c
Putra Mahardika <putram.works@gmail.com>
2024-10-18 13:06:59 +0700

    Update typeDefs.js

--- server/follow/typeDefs.js
+++ server/follow/typeDefs.js
@@ -1,14 +1,14 @@
 const followTypeDefs = `#graphql
   type Follow {
-    _id: ID!
-    followingUser: User!
-    followerUser: User!
-    createdAt: String!
-    updatedAt: String!
+    _id: ID!                # Unique identifier for the follow relationship
+    followingUser: User!    # User that is being followed
+    followerUser: User!      # User that is following
+    createdAt: String!      # Timestamp of when the follow relationship was created
+    updatedAt: String!      # Timestamp of the last update to the follow relationship
   }
 
   type Mutation {
-    followUser(followingId: ID!): String
+    followUser(followingId: ID!): String   # Follow a user by their ID and return a success message
   }
 `;
 
