03f7762fe6d1ffc62baea56084ccd4225bccc410
Putra Mahardika <putram.works@gmail.com>
2024-10-18 13:22:44 +0700

    Update model.js

--- server/user/model.js
+++ server/user/model.js
@@ -3,36 +3,38 @@ const { ObjectId } = require("mongodb");
 const { hashPassword } = require("../helpers/bcrypt");
 
 class UserModel {
+  // Create a new user
   static async create(payload) {
     const db = await connectDB();
 
+    // Validate password length
     if (payload.password.length < 6) {
       throw new Error("Password minimal 6 karakter");
     }
 
+    // Hash the password
     payload.password = hashPassword(payload.password);
-
+    
+    // Set createdAt and updatedAt fields
     payload.createdAt = new Date();
     payload.updatedAt = new Date();
 
     const result = await db.collection("users").insertOne(payload);
 
-    const newUser = await db
-      .collection("users")
-      .findOne({ _id: result.insertedId });
-
-    return newUser;
+    // Fetch the newly created user
+    return await db.collection("users").findOne({ _id: result.insertedId });
   }
 
+  // Find a user by username
   static async findByUsername(username) {
     const db = await connectDB();
-
     return await db.collection("users").findOne({ username });
   }
 
+  // Search for users by name or username
   static async searchUsers(keyword) {
     const db = await connectDB();
-
+    
     const query = {
       $or: [
         { name: { $regex: keyword, $options: "i" } },
@@ -43,6 +45,7 @@ class UserModel {
     return await db.collection("users").find(query).toArray();
   }
 
+  // Find a user by their ID
   static async findById(id) {
     const db = await connectDB();
 
@@ -55,41 +58,37 @@ class UserModel {
     }
 
     try {
-      const user = await db
-        .collection("users")
-        .aggregate([
-          {
-            $match: { _id: new ObjectId(id) },
+      const user = await db.collection("users").aggregate([
+        {
+          $match: { _id: new ObjectId(id) },
+        },
+        {
+          $lookup: {
+            from: "posts",
+            localField: "_id",
+            foreignField: "authorId",
+            as: "userPosts",
           },
-          {
-            $lookup: {
-              from: "posts",
-              localField: "_id",
-              foreignField: "authorId",
-              as: "userPosts",
-            },
+        },
+        {
+          $lookup: {
+            from: "follows",
+            localField: "_id",
+            foreignField: "followingId",
+            as: "followers",
           },
-          {
-            $lookup: {
-              from: "follows",
-              localField: "_id",
-              foreignField: "followingId",
-              as: "followers",
-            },
+        },
+        {
+          $lookup: {
+            from: "follows",
+            localField: "_id",
+            foreignField: "followerId",
+            as: "following",
           },
-          {
-            $lookup: {
-              from: "follows",
-              localField: "_id",
-              foreignField: "followerId",
-              as: "following",
-            },
-          },
-        ])
-        .next();
+        },
+      ]).next();
 
       console.log("User data retrieved:", user);
-
       return user;
     } catch (error) {
       console.error("Error fetching user by ID:", error);
